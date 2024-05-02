16582f2a56652a06c7db4d9066ae461aaf60422a
Luke859 <71589052+Luke859@users.noreply.github.com>
2024-05-02 17:04:06 +0200

    ajout Mvc

---
+++ src/test/java/com/sip/ams/TestDemoDataController.java
@@ -0,0 +1,12 @@
+package com.sip.ams;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+public class TestDemoDataController {
+    //
+}
