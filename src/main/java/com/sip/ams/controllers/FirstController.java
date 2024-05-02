2b2290bcab8e2c784fd9807e1693c458fbdb9cdc
Luke859 <71589052+Luke859@users.noreply.github.com>
2024-05-02 12:27:04 +0200

    first commit

---
+++ src/main/java/com/sip/ams/controllers/FirstController.java
@@ -0,0 +1,21 @@
+package com.sip.ams.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+	@GetMapping("/infourl")
+	// @ResponseBody
+	public String information() {
+		return "first/info";
+	}
+
+	@GetMapping("/details")
+	// @ResponseBody
+	public String detailsFormation() {
+		return "first/details";
+	}
+
+}
